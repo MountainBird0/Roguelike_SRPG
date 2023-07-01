6d3dcbe174393a428b0f9f4dfcb786e3bec912e0
Kim Jae Hwi <99602963+MountainBird0@users.noreply.github.com>
2023-07-01 23:46:31 +0900

    [23.07.01] Add UnitSlot
    유닛슬롯만듬

--- Assets/02_Scripts/Scene/BattleMap/BattleMapInput.cs
+++ Assets/02_Scripts/Scene/BattleMap/BattleMapInput.cs
@@ -8,16 +8,15 @@ using UnityEngine.Tilemaps;
 
 public class BattleMapInput : MonoBehaviour
 {
-    Ray ray;
-    RaycastHit2D hit;
-
+    // test
     [SerializeField]
     private Tilemap tilemap;
     [SerializeField]
     private Tilemap hight;
     [SerializeField]
-
     private TileBase newTile;
+
+    public GameObject player;
 
     private void OnEnable()
     {
@@ -32,13 +31,17 @@ public class BattleMapInput : MonoBehaviour
     private void ScreenTouch(Vector2 screenPosition, float time)
     {
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        worldPosition.x += 0.5f;
+        worldPosition.y += 0.5f;
         Vector3Int cellPosition = tilemap.WorldToCell(worldPosition);
+
         TileBase tile = tilemap.GetTile(cellPosition);
 
         if (tile != null)
         {
-            Debug.Log($"{GetType()} - 터치한 타일: {tile.name}");
+            Debug.Log($"{GetType()} - 터치한 타일: {tile.name}, {tile}");
             hight.SetTile(cellPosition, newTile);
+            player.transform.position = cellPosition;
         }
     }
 }
